0b867faf523e5afb1e060a10006b2717eb308edd
Timur <Kuznetsov5132@gmail.com>
2022-02-28 00:09:56 +0300

    Update HorizontalLine.cs

--- Snake/HorizontalLine.cs
+++ Snake/HorizontalLine.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    internal class HorizontalLine
+    class HorizontalLine
     {
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xReight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xReight; x++) 
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
     }
 }
