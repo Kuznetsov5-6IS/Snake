07c2ab483b65eeb62496182bead6e955990077ff
Timur <Kuznetsov5132@gmail.com>
2022-02-27 23:45:50 +0300

    Update Program.cs

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -18,6 +18,20 @@ namespace Snake
 
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
             Console.ReadLine();
         }
     }
